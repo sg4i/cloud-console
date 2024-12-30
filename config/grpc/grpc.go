c4276ab9c3d0a3a52fe55c91bb8dc0de70ba2080
sg4i <sg-i@outlook.com>
2024-12-30 19:46:08 +0800

    rpc config

--- config/grpc/grpc.go
+++ config/grpc/grpc.go
@@ -5,15 +5,27 @@ import (
 )
 
 type GRPCConfig struct {
-	Token string
+	Token       string
+	RPCAddress  string
+	HTTPAddress string
 }
 
 func New() *GRPCConfig {
 	return &GRPCConfig{
-		Token: utils.GetString("rpc.token"),
+		Token:       utils.GetString("grpc.token"),
+		RPCAddress:  utils.GetString("grpc.rpcAddress"),
+		HTTPAddress: utils.GetString("grpc.httpAddress"),
 	}
 }
 
 func (c *GRPCConfig) GetToken() string {
 	return c.Token
+}
+
+func (c *GRPCConfig) GetRPCAddress() string {
+	return c.RPCAddress
+}
+
+func (c *GRPCConfig) GetHTTPAddress() string {
+	return c.HTTPAddress
 }
